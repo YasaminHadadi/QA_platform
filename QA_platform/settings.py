8e80abe25f6a67a8561aca2a310fb36725059b2e
yasmin <yasamin@gmail.com>
2025-07-02 09:05:15 +0330

    Configure JWT token lifetimes in settings.py

--- QA_platform/settings.py
+++ QA_platform/settings.py
@@ -131,3 +131,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
