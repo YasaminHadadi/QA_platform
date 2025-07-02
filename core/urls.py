f649cdc392fce0996637fecd6e87fdc8ee538e8f
yasmin <yasamin@gmail.com>
2025-07-02 08:41:40 +0330

    Update question and answer URL paths

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('question/', views.QuestionListCreateView.as_view(), name='question-list'),
+    path('questions/', views.QuestionListCreateView.as_view(), name='question-list'),
     path('question/<int:pk>/', views.QuestionDetailView.as_view(), name='question-detail'),
     path('answers/', views.AnswerListCreateView.as_view(), name='answer-list'),
-    path('answers/<int:pk>/', views.AnswerListCreateView.as_view(), name='answer-detail'),
+    path('answer/<int:pk>/', views.AnswerDetailView.as_view(), name='answer-detail'),
 ]
